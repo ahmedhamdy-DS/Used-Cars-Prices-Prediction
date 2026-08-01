e3438bf224b65d93c9fc67b2010e72fcb22f5e91
Run Time Error <zazadragonnp@gmail.com>
2026-08-01 12:46:30 +0300

    Implement CORS middleware in FastAPI app
    Add CORS middleware to FastAPI for cross-origin requests.

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ from pydantic import BaseModel
 import joblib
 import xgboost as xgb
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Car Price Prediction API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Car Price Prediction API! Go to /docs to test the model."}
-# 1. تحميل الـ Pipeline بتاع معالجة البيانات
+  
 pipeline = joblib.load("preprocessing_pipeline.pkl")
 
-# 2. تحميل موديل XGBoost
 model = xgb.XGBRegressor()
 model.load_model("XGBR.json")
 
-# 3. تحديد شكل البيانات اللي الـ API هيستقبلها (بدون عمود price)
 class ModelInput(BaseModel):
     region: str
     year: int
@@ -34,13 +41,10 @@ class ModelInput(BaseModel):
 
 @app.post("/predict")
 def predict(data: ModelInput):
-    # تحويل البيانات لـ DataFrame عشان الـ Pipeline يقدر يتعامل معاها
     input_df = pd.DataFrame([data.model_dump()])
     
-    # 1. تمرير البيانات على الـ Pipeline عشان يحصلها Preprocessing
     processed_data = pipeline.transform(input_df)
     
-    # 2. تمرير البيانات المعالجة للموديل لاستخراج التوقع
     prediction = model.predict(processed_data)
     
     return {
