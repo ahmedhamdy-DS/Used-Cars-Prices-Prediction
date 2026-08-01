3f48543e2dfc9a0bc1111aed99e53b261a808d95
Run Time Error <zazadragonnp@gmail.com>
2026-08-01 13:46:40 +0300

    Enhance predict function for data processing
    Refactor prediction endpoint to handle input data processing and type conversions.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import joblib
 import xgboost as xgb
 import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
-
 app = FastAPI(title="Car Price Prediction API")
 app.add_middleware(
     CORSMiddleware,
@@ -14,14 +13,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Car Price Prediction API! Go to /docs to test the model."}
-  
+
 pipeline = joblib.load("preprocessing_pipeline.pkl")
 
 model = xgb.XGBRegressor()
 model.load_model("XGBR.json")
+
 
 class ModelInput(BaseModel):
     region: str
@@ -39,15 +40,13 @@ class ModelInput(BaseModel):
     paint_color: str
     state: str
 
-import pandas as pd
-
 @app.post("/predict")
 def predict(data: ModelInput):
     
-    
+    # 1. استلام البيانات
     input_df = pd.DataFrame([data.model_dump()])
     
-   
+
     expected_columns = [
         'region', 'year', 'manufacturer', 'model', 'condition', 
         'cylinders', 'fuel', 'odometer', 'title_status', 'transmission', 
@@ -55,15 +54,25 @@ def predict(data: ModelInput):
     ]
     input_df = input_df[expected_columns]
     
-  
+
     input_df['condition_missing'] = 0
+   
     input_df['cylinders'] = input_df['cylinders'].astype(str).str.extract(r'(\d+)').astype(float)
     input_df['cylinders'] = input_df['cylinders'].fillna(6.0)
+    input_df['year'] = input_df['year'].astype(float)
+    input_df['odometer'] = input_df['odometer'].astype(float)
     
 
+    categorical_cols = [
+        'region', 'manufacturer', 'model', 'condition', 'fuel', 
+        'title_status', 'transmission', 'drive', 'type', 'paint_color', 'state'
+    ]
+    for col in categorical_cols:
+        input_df[col] = input_df[col].astype('category')
+ 
     processed_data = pipeline.transform(input_df)
     
-   
+
     prediction = model.predict(processed_data)
     
     return {
