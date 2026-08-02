ff55b7c76a03bb0f756e0fc4b9e946c60e7ea475
ahmed <zazadragonnp@gmail.com>
2026-08-02 19:06:36 +0300

    fixed

--- main.py
+++ main.py
@@ -57,6 +57,8 @@ ALLOWED_ORIGINS = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
+
+    allow_origin_regex=r"^https://used-cars-prices-prediction.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
